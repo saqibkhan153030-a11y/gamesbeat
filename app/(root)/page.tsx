ba013ab1eb8269fed509cde481df7529a0e1e66e
saqibkhan153030-a11y <saqibkhan153030@gmail.com>
2026-03-24 08:50:04 +0500

    new section Newgames

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import HomeHero from "./components/HomeHero";
 import MostPlayedGames from "./components/MostPlayedGames";
+import NewGames from "./components/NewGames";
 export default function Home() {
   return (
     <>
       <HomeHero />
       <MostPlayedGames/>
+      <NewGames/>
       
     </>
   );
