dba321bb5dc06fb2e90ab8d965f9a383ec56c915
saqibkhan153030-a11y <saqibkhan153030@gmail.com>
2026-04-08 18:05:25 +0500

    work on AdventuresExample section

--- app/(root)/adventures/page.tsx
+++ app/(root)/adventures/page.tsx
@@ -1,9 +1,11 @@
 import AdventuresHero from "./components/AdventuresHero"
+import AdventuresExample from "./components/AdventuresExample"
 
 export default function Adventures() {
     return (
         <div>
          <AdventuresHero/>
+         <AdventuresExample/>
         
         </div>
     )
